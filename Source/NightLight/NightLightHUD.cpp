dc5f9f50c577dee8acd0ec00fddcc215967becf8
Sena <sena0711@gmail.com>
2017-07-18 17:02:24 +1200

    Gun mesh migrated

--- Source/NightLight/NightLightHUD.cpp
+++ Source/NightLight/NightLightHUD.cpp
@@ -10,7 +10,7 @@
 ANightLightHUD::ANightLightHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Assets/FirstPerson/Textures/FirstPersonCrosshair"));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
