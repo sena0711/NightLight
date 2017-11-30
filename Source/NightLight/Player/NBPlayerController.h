dace728987dbd349b69446bedd2c52e800a56060
Sena Kim <sena0711@gmail.com>
2017-11-30 19:58:17 +1300

    added playercontroller cpp and base ai cpp. empty

---
+++ Source/NightLight/Player/NBPlayerController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "NBPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NIGHTLIGHT_API ANBPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
