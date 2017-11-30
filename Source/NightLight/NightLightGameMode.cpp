f54a68b0fe7f633bc678eee3280f60b007e2f197
Sena Kim <sena0711@gmail.com>
2017-11-30 15:17:22 +1300

    New Project Private 4.18

--- Source/NightLight/NightLightGameMode.cpp
+++ Source/NightLight/NightLightGameMode.cpp
@@ -1,17 +1,15 @@
 // Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
 
 #include "NightLightGameMode.h"
-#include "NightLightHUD.h"
 #include "NightLightCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 ANightLightGameMode::ANightLightGameMode()
-	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Blueprints/FirstPersonCharacter"));
-	DefaultPawnClass = PlayerPawnClassFinder.Class;
-
-	// use our custom HUD class
-	HUDClass = ANightLightHUD::StaticClass();
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
 }
