dc4fb27afb2b8c213da36e83ccbe45e620563542
Sena Kim <sena0711@gmail.com>
2018-01-12 14:03:59 +1300

    made base weapon instance for effects

---
+++ Source/NightLight/Assets/BaseWeaponInstance.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Assets/BaseWeapon.h"
+#include "BaseWeaponInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NIGHTLIGHT_API ABaseWeaponInstance : public ABaseWeapon
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ABaseWeaponInstance();
+
+
+
+};
