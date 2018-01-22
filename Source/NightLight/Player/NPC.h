44d694918fe9c13674941a6a5fb12c268745a73f
Sena Kim <sena0711@gmail.com>
2018-01-22 18:04:42 +1300

    changed some map content sounds. and npc c++ made for nurse

---
+++ Source/NightLight/Player/NPC.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Player/BaseCharacter.h"
+#include "NPC.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NIGHTLIGHT_API ANPC : public ABaseCharacter
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
